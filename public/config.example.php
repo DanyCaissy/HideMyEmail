2237c7e182f95e1a932b0136146d7d1f7190367e
Dany <dany.caissy@gmail.com>
2019-04-02 22:53:58 -0500

    Added config example file

---
+++ public/config.example.php
@@ -0,0 +1,19 @@
+<?php
+
+	$productionCredentials = array(
+		"host" => "127.0.0.1",
+		"user" => "user",
+		"password" => "pass",
+		"database" => "hidemyemail",
+		"port" => null,
+		"socket" => null
+	);
+
+	$localCredentials = array(
+		"host" => "127.0.0.1",
+		"user" => "user",
+		"password" => "pass",
+		"database" => "hidemyemail",
+		"port" => null,
+		"socket" => null
+	);
